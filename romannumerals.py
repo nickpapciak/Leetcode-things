7d37da24747685558f87e4cc652d3ddf939ebf2a
Nicholas Papciak <nicholaspapciak@gmail.com>
2022-08-10 12:29:13 -0400

    clarified  some stuff

--- romannumerals.py
+++ romannumerals.py
@@ -1,4 +1,5 @@
 import functools
+# ^ you can make it faster at expense of memory
 
 NUMERAL_MAP = {"M":1000, "CM":900, "D":500, "CD":400, "C":100, "XC":90, "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I":1}
 
@@ -9,19 +10,20 @@ def int_to_roman(i: int) -> str:
         quo, rem = divmod(i, traditional)
         if quo > 0: 
             return (quo * roman) + int_to_roman(rem)
+    # base case
     return ""
 
 @functools.cache
 def roman_to_int(r: str) -> int:
+    # base case
     if r == '': 
         return 0
-
+    # if there is a special subtraction pair
     if r[:2] in NUMERAL_MAP.keys(): 
-        # if there is a special subtraction pair
         return NUMERAL_MAP[r[:2]] + roman_to_int(r[2:])
+    # otherwise single numeral
     else: 
-        # otherwise regular numeral
         return NUMERAL_MAP[r[0]] + roman_to_int(r[1:])
 
-
-# print(any([x == roman_to_int(int_to_roman(x)) for x in range(1000000)]))
+# tests all traditional roman numerals
+assert any([x == roman_to_int(int_to_roman(x)) for x in range(1, 4000)]), "numeral failed"
